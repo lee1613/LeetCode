7528e41ee244ecf9f58d0dfca1384982f3288a50
lee1613 <100192154+lee1613@users.noreply.github.com>
2025-08-15 13:08:36 +0200

    Time: 0 ms (100%), Space: 17.6 MB (98.32%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,13 +1,23 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
 
+        # Solution 1 (Building integer from sratch)
+        # if n < 1:
+        #     return False
+
+        # curr = 1
+        # while curr < n:
+        #     curr *= 4
+        # if curr == n:
+        #     return True
+        # else: 
+        #     return False
+
+        # Solution 2 (Check if the log (base 4) of a number is integer)
         if n < 1:
             return False
-
-        curr = 1
-        while curr < n:
-            curr *= 4
-        if curr == n:
+        res = math.log(n, 4)
+        if res - int(res) < 1 * 10**-10:
             return True
-        else: 
+        else:
             return False
