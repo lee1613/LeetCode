a15ea9c398caa533633b9b3b208294c316ef7827
lee1613 <100192154+lee1613@users.noreply.github.com>
2025-08-19 09:06:34 +0200

    Time: 23 ms (96.89%), Space: 28.5 MB (42.33%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -1,29 +1,31 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
 
-        # Counting at each loop
-        res = 0
-        count = 0
-        for num in nums: 
-            if num == 0:
-                count += 1
-                res += count
-            else:
-                count = 0
-
-        return res
-
-        # Counting after the streak stop
-
+        # Counting at each loop (29ms, 28.39mb)
         # res = 0
         # count = 0
         # for num in nums: 
         #     if num == 0:
         #         count += 1
-                
+        #         res += count
         #     else:
-        #         if count > 0:
-        #             res += count * (count + 1) / 2
-        #             count = 0
+        #         count = 0
 
         # return res
+
+        # Counting after the streak stop ()
+
+        res = 0
+        count = 0
+        for num in nums: 
+            if num == 0:
+                count += 1
+                
+            else:
+                if count > 0:
+                    res += int(count * (count + 1) / 2)
+                    count = 0
+
+        res += int(count * (count + 1) / 2)
+
+        return res
