68bbc68efed9e9cae4ad2a70aa2d9ee129e417f8
lee1613 <100192154+lee1613@users.noreply.github.com>
2025-08-21 16:01:20 +0200

    Time: 721 ms (15.58%), Space: 19.1 MB (5.3%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.py
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.py
@@ -2,71 +2,108 @@ class Solution:
     def numSubmat(self, mat: List[List[int]]) -> int:
         res = 0
         sub = [[0] * (len(mat[0]) + 1) for i in range(len(mat) +1)]
+        largest = 0
 
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if mat[i][j] == 1:
                     sub[i + 1][j + 1] = min(sub[i][j], sub[i + 1][j], sub[i][j + 1]) + 1
+                    if sub[i + 1][j + 1] > largest:
+                        largest = sub[i + 1][j + 1]
+
+        print(sub)
         
+
         res += sum(list(map(sum, sub)))
         
         
-        for i in range(1, len(sub)):
-            prev = 0
-            count = 0
-            for j in range(1, len(sub[0])): 
-                if sub[i][j] > 1:
-                    if sub[i][j] == prev:
-                        count += 1
-                        res += count - 1
+        
+        # for i in range(1, len(sub)):
+        #     prev = 0
+        #     count = 0
+        #     for j in range(1, len(sub[0])): 
+        #         if sub[i][j] > 1:
+        #             if sub[i][j] == prev:
+        #                 count += 1
+        #                 res += count - 1
                     
+        #             else:
+        #                 count = 1
+        #                 prev = sub[i][j]
+
+        #         else:
+        #             prev = sub[i][j]
+        #             count = 0
+        cum = 1
+        while cum <= largest:
+            for i in range(len(sub) - 1, 0, -1):
+                count = 0
+                for j in range(len(sub[0]) - 1, 0, -1): 
+                    if sub[i][j] >= cum:
+                        count += 1
+
                     else:
-                        count = 1
-                        prev = sub[i][j]
+                        res += int(count * (count - 1) / 2)
+                        count = 0
+                
+                res += int(count * (count - 1) / 2)
 
-                else:
-                    prev = sub[i][j]
-                    count = 0
-                    
+            # print(res)
 
-        
-        
-
-        for j in range(1, len(sub[0])):
-            prev = 0
-            count = 0
-            for i in range(1, len(sub)): 
-                if sub[i][j] > 1:
-                    if sub[i][j] == prev:
+            for j in range(len(sub[0]) - 1, 0, -1):
+                count = 0
+                for i in range(len(sub) - 1, 0, -1): 
+                    if sub[i][j] >= cum:
                         count += 1
-                        res += count - 1
+
+                    else:
+                        res += int(count * (count - 1) / 2)
+                        count = 0
+                
+                res += int(count * (count - 1) / 2)
+            
+            cum += 1
+                    
+        # print(res)
+        # res += sum(list(map(sum, sub)))
+        
+        
+
+        # for j in range(1, len(sub[0])):
+        #     prev = 0
+        #     count = 0
+        #     for i in range(1, len(sub)): 
+        #         if sub[i][j] > 1:
+        #             if sub[i][j] == prev:
+        #                 count += 1
+        #                 res += count - 1
                         
-                    else:
-                        prev = sub[i][j]
-                        count = 1
-                else:
-                    count = 0
-                    prev = sub[i][j]
+        #             else:
+        #                 prev = sub[i][j]
+        #                 count = 1
+        #         else:
+        #             count = 0
+        #             prev = sub[i][j]
 
                 
 
-        for i in range(len(mat)):
-            count = 0
-            for j in range(len(mat[0])):
-                if mat[i][j] == 1:
-                    count += 1
-                    res += max(count - 1, 0)
-                else:
-                    count = 0
+        # for i in range(len(mat)):
+        #     count = 0
+        #     for j in range(len(mat[0])):
+        #         if mat[i][j] == 1:
+        #             count += 1
+        #             res += max(count - 1, 0)
+        #         else:
+        #             count = 0
 
-        for j in range(len(mat[0])):
-            count = 0
-            for i in range(len(mat)):
-                if mat[i][j] == 1:
-                    count += 1
-                    res += max(count - 1, 0)
-                else:
-                    count = 0
+        # for j in range(len(mat[0])):
+        #     count = 0
+        #     for i in range(len(mat)):
+        #         if mat[i][j] == 1:
+        #             count += 1
+        #             res += max(count - 1, 0)
+        #         else:
+        #             count = 0
 
         return res
         
