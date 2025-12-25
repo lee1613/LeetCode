0067022d734870d3bf3781126f35ac739f037d70
lee1613 <100192154+lee1613@users.noreply.github.com>
2025-12-25 17:02:01 +0800

    Time: 26 ms (49.95%), Space: 22.7 MB (99.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,31 +1,40 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = [nums[0]]
-        suffix = [nums[-1]]
 
         n = len(nums)
-
+        res = [0] * n
         # Any edge cases where the nums.length  = 2 is resolved
 
-        for i in range(1, n):
-            prefix.append(nums[i] * prefix[-1])
+        res[-1] = nums[-1]
+        for i in range(n - 2, 0, -1):
+            res[i] = nums[i] * res[i + 1]
         
-        for i in range(n - 2, -1, -1):
-            suffix.append(nums[i] * suffix[-1])
+        res[0] = res[1]
 
+        product = 1
 
+        for i in range(1, n - 1):
+            product *= nums[i - 1]
+            res[i] = product * res[i + 1]
         
-        # The first result is the index - -2 of suffix, then the second results is the product of index - 0 of prefix * index - -3 of suffix, 
-
-        res = [suffix[-2]]
-
-        for i in range(n - 2):
-            # Multiply with the third last suffix product when it's the first one since the second element is being ignored
-            res.append(suffix[n - 3  - i] * prefix[i])
-
-            # The last element will be prefix of n-3th element multiply with the 0th elmeent of suffix
-        res.append(prefix[-2])
+        res[-1] = product * nums[-2]
 
         return res
+        
+
+
+
+        
+        # # The first result is the index - -2 of suffix, then the second results is the product of index - 0 of prefix * index - -3 of suffix, 
+
+
+        # for i in range(n - 2):
+        #     # Multiply with the third last suffix product when it's the first one since the second element is being ignored
+        #     res.append(suffix[n - 3  - i] * prefix[i])
+
+        #     # The last element will be prefix of n-3th element multiply with the 0th elmeent of suffix
+        # res.append(prefix[-2])
+
+        # return res
 
         
